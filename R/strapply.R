3e5038fb3f1c146148b09b9989b997096b8bd587
G. Grothendieck <ggrothendieck@gmail.com>
2006-10-13 00:00:00 +0000

    version 0.2-2

--- R/strapply.R
+++ R/strapply.R
@@ -1,27 +1,15 @@
-
-#strapply <- function(X, pattern, FUN = function(x, ...) x, ..., 
-#   simplify = FALSE, USE.NAMES = FALSE)
-#      sapply(X, function(x) {
-#          v <- NULL
-#	  gsubfn(pattern, function(x, ...) v <<- c(v, FUN(x, ...)), x, ...)
-#	  v
-#	  }, simplify = simplify, USE.NAMES = USE.NAMES)
-
-
 strapply <- 
 function (X, pattern, FUN = function(x, ...) x, ..., simplify = FALSE, 
-    USE.NAMES = FALSE) {
-	# if (inherits(FUN, "formula")) FUN <- as.function(FUN)
-	FUN <- match.funfn(FUN)
-	result <- sapply(X, function(x) {
-	    first <- TRUE
-	    v <- NULL
-	    gsubfn(pattern, function(x, ...)
-		v <<- if (first) { first <<- FALSE; FUN(x, ...) } 
-		else c(v, FUN(x, ...)),
-		x, ...)
-	    v
-	}, simplify = is.logical(simplify) && simplify, USE.NAMES = USE.NAMES)
-	if (is.logical(simplify)) result else do.call(simplify, result)
+    USE.NAMES = FALSE, combine = c) 
+{
+    FUN <- match.funfn(FUN)
+    result <- sapply(X, function(x) {
+        v <- NULL
+        gsubfn(pattern, function(x, ...) 
+		v <<- c(v, combine(FUN(x, ...))), x, ...)
+	v
+    }, simplify = is.logical(simplify) && simplify, USE.NAMES = USE.NAMES)
+    if (is.logical(simplify)) 
+        result
+    else do.call(simplify, result)
 }
-
